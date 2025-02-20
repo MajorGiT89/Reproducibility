eef2d2062b31b418a7072398be3c21dcaf204551
MajorGiT89 <VTLNIC004@myuct.ac.za>
2025-02-20 10:40:59 +0200

    Removal and update

--- Analysis.R
+++ Analysis.R
@@ -2,7 +2,9 @@ rm(list = ls())
 
 setwd("C:/Users/Admin/Documents/UCT_2025/GIT/Honours-Project")
 
-# Load Packages ---------------------------------------------------------
+### !!! NOTE: use Alt + O to close all folded code sections for easy navigation !!! (use Alt + shft + O to open all) ###
+
+#1 Load Packages ---------------------------------------------------------
 
 #Load
 
@@ -11,9 +13,9 @@ library("rio")
 library("visreg")
 library("gridExtra")
 
-# Load Data -------------------------------------------------
 
-# Licencing and Referencing of Data ----------------------------------------
+
+#2 Licencing and Referencing of Source Data ----------------------------------------
 
 #Copyright © 2022 Fish, Wolf, Smeltz, Harris and Planas. This is an open-access article distributed under the terms of the Creative Commons Attribution License (CC BY). The use, distribution or reproduction in other forums is permitted, provided the original author(s) and the copyright owner(s) are credited and that the original publication in this journal is cited, in accordance with accepted academic practice. No use, distribution or reproduction is permitted which does not comply with these terms.
 
@@ -22,4 +24,14 @@ library("gridExtra")
 #Frontiers in Marine Science 2022 Vol. 9 
 #DOI: 10.3389/fmars.2022.801759
 #https://www.frontiersin.org/journals/marine-science/articles/10.3389/fmars.2022.801759
+
+
+
+#3 Load Data -------------------------------------------------
+
+odat <- import("Halibut_OocyteCycle_Raw.csv")
+
+
+#4 Data Cleaning Process --------------------------------------------------------------
+
 
