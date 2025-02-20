3c8e3a26aca53147140a3251aa8c06e52ec18a39
MajorGiT89 <VTLNIC004@myuct.ac.za>
2025-02-20 07:55:25 +0200

    Data + Analysis Script update

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,16 @@
+rm(list = ls())
+
 setwd("C:/Users/Admin/Documents/UCT_2025/GIT/Honours-Project")
+
+# Load Packages ---------------------------------------------------------
+
+#Load
+
+library("tidyverse")
+library("rio")
+library("visreg")
+library("gridExtra")
+
+# Load Data -------------------------------------------------
+
+fishdat <- read.csv("Capture_Quantity.csv") 
