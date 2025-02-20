25b5cc54bdd19ca324c083450f59d857b9439f67
MajorGiT89 <VTLNIC004@myuct.ac.za>
2025-02-20 10:40:20 +0200

    Update on File and analysis

--- Analysis.R
+++ Analysis.R
@@ -13,4 +13,13 @@ library("gridExtra")
 
 # Load Data -------------------------------------------------
 
+# Licencing and Referencing of Data ----------------------------------------
+
+#Copyright © 2022 Fish, Wolf, Smeltz, Harris and Planas. This is an open-access article distributed under the terms of the Creative Commons Attribution License (CC BY). The use, distribution or reproduction in other forums is permitted, provided the original author(s) and the copyright owner(s) are credited and that the original publication in this journal is cited, in accordance with accepted academic practice. No use, distribution or reproduction is permitted which does not comply with these terms.
+
+#Reproductive Biology of Female Pacific Halibut (Hippoglossus stenolepis) in the Gulf of Alaska
+#T. Fish, N. Wolf, T. S. Smeltz, B. P. Harris and J. V. Planas
+#Frontiers in Marine Science 2022 Vol. 9 
+#DOI: 10.3389/fmars.2022.801759
+#https://www.frontiersin.org/journals/marine-science/articles/10.3389/fmars.2022.801759
 
